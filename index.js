3d25a0f056f259dcd8671d520381e406affedad0
Ihor Reli <i.reli@ualwo9605nbl.levi9.com>
2022-02-02 09:30:51 +0200

    removed cors

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const currencies = require('./currencies');
 
 const app = express();
+const allowCrossDomain = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' === req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
+};
+
+app.use(allowCrossDomain);
 
 app.get('/currencies', (req, res) => {
     res.json(currencies);
